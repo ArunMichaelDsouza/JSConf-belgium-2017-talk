45aace36441afb45b9c1ecd28fbd7707dc656c30
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-06-25 17:13:11 +0530

    Webpack demo added

---
+++ demos/webpack-demo/README.md
@@ -0,0 +1,11 @@
+## Installation
+
+```javascript
+npm install -g webpack
+```
+
+## Usage
+
+```javascript
+webpack app.js bundle.js
+```
---
+++ demos/webpack-demo/math.js
@@ -0,0 +1,10 @@
+var math = {
+	sum: function(a, b) {
+		return a + b;
+	},
+	product: function(a, b) {
+		return a * b;
+	}
+};
+
+module.exports = math;
